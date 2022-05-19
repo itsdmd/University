240625aa4e0568caa8aef006a8247b6bb6956cdd
Đức Đào <duc.dao.431@gmail.com>
2022-05-19 17:21:44 +0700

    [add] 107.L.02-02
    Fully functional

---
+++ CS107/exc/L/W02/FractionsFromKeyboard/w02-02.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include "src/io.h"
+#include "../src/frac.h"
+#include "../src/rand.h"
+
+using std::cout;
+using std::vector;
+
+
+
+/// -------------------------------- main() -------------------------------- ///
+int main() {
+	int size;
+	vector<Frac> vt_f;
+	Frac f_res;
+	
+	cout << "Working with randomly generated integers.\n";
+	
+	/// --------------------------------- Input -------------------------------- ///
+	srand(time(NULL));
+	size = rng(2, 4);
+	
+	cout << "Please enter " << size << " fraction";
+	(size > 1) ? cout << "s:\n" : cout << ":\n";
+	
+	multiInput(vt_f, size);
+	
+
+	/// -------------------------------- Output -------------------------------- ///
+	fracVctSum(vt_f, f_res);
+	
+	cout << "The sum of all entered fractions is: ";
+	printFrac(f_res);
+	
+	
+	return 0;
+}
