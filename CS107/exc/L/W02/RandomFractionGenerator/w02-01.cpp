b8ac1d99ebb5bc642767024ff4da97049412d377
Đức Đào <duc.dao.431@gmail.com>
2022-05-25 15:15:03 +0700

    107.L.02-01.03
    Adopt FracVct class

--- CS107/exc/L/W02/RandomFractionGenerator/w02-01.cpp
+++ CS107/exc/L/W02/RandomFractionGenerator/w02-01.cpp
@@ -11,7 +11,6 @@ using std::vector;
 int main() {
 	int size;
 	vector<int> vt1, vt2;
-	vector<Frac> vt_f;
 	
 	cout << "Working with randomly generated integers.\n";
 	
@@ -22,20 +21,20 @@ int main() {
 	rndVct(vt1, size, 10, 100);
 	rndVct(vt2, size, 10, 100);
 
-	fracVctGen(vt1, vt2, vt_f);
+	FracVct vt = FracVct(vt1, vt2);
 
 	/// -------------------------------- Output -------------------------------- ///
 	cout << "Generating " << size << " random fractions: ";
-	printFVct(vt_f);
+	vt.printFVct();
 	
 	cout << "Fractions in their lowest term: ";
-	printFVctReduced(vt_f);
+	vt.printFVctReduced();
 	
 	cout << "Fractions in their decimal form: ";
-	printFVctDec(vt_f);
+	vt.printFVctDec();
 	
 	cout << "Fractions in their percentage form: ";
-	printFVctPct(vt_f);
+	vt.printFVctPct();
 	
 	
 	return 0;
