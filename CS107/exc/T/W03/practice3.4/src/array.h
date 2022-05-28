59764eb3156b11696708db5d3d7de3a044d180b7
Đức Đào <duc.dao.431@gmail.com>
2022-05-28 15:24:21 +0700

    107.T.03-02.src.array
    Intergrate array.cpp into array.h since using template<>

--- CS107/exc/T/W03/practice3.4/src/array.h
+++ CS107/exc/T/W03/practice3.4/src/array.h
@@ -7,15 +7,56 @@ private:
 	int size;
 	
 public:
-	Array();
-	Array(int const &size);
-	Array(Array const &other);
-	~Array();
+	Array() {
+		this->size = 0;
+		this->data = nullptr;
+	};
 	
-	T &getVal(int const &index);
-	int getSize();
+	Array(int const &size) {
+		this->data = new T[size];
+		this->size = size;
+	};
 	
-	void setSize(int const &size);
+	Array(Array const &other) {
+		this->data = new T[other.size];
+		this->size = other.size;
+		
+		for (int i = 0; i < this->size; i++) {
+			this->data[i] = other.data[i];
+		}
+	};
+	
+	~Array() {
+		delete[] this->data;
+	};
+	
+	
+	T &getVal(int const &index) {
+		return this->data[index];
+	};
+	
+	int getSize() {
+		return this->size;
+	};
+	
+	
+	void setSize(int const &size) {
+		int oldSize = this->size;
+		T *oldData = this->data;
+		
+		this->data = new T[size];
+		this->size = size;
+		
+		for (int i = 0; i < size; i++) {
+				if (i < oldSize) {
+					this->data[i] = oldData[i];
+				}
+				
+				else {
+					this->data[i] = T();
+				}
+			}
+		
+		delete[] oldData;
+	};
 };
-
-#include "array.cpp"
