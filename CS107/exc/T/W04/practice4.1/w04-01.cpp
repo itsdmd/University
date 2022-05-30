1df2be5a3b2c1d2514daa422ebaa4179460a0e0e
Đức Đào <duc.dao.431@gmail.com>
2022-05-30 19:12:43 +0700

    [add] 107.T.04-01
    Fully functional

---
+++ CS107/exc/T/W04/practice4.1/w04-01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "src/frac.h"
+
+using std::cout;
+using std::cin;
+
+
+int main() {
+	Frac f1(1, 2);
+	Frac f2(2, 3);
+	
+	Frac f3 = ((f1++) + f2);
+	Frac f4 = (f1 * (--f2));
+	
+	cout << f3 << "\n";
+	cout << f4 << "\n";
+	
+	cout << (f3 < f4) << "\n";
+	
+	cout << (f3 += f4).reduce() << "\n";
+	
+	f1 = f2 = f3 = f4;
+	
+	cout << f1 << " " << f2 << " " << f3 << " " << f4 << "\n";
+	
+	
+	return 0;
+}
