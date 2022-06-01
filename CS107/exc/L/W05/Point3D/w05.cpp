ce581cf97286c6a87f49fc57650aa5692dce4908
Đức Đào <duc.dao.431@gmail.com>
2022-06-01 20:35:34 +0700

    107.L.05.01
    Fully functional

--- CS107/exc/L/W05/Point3D/w05.cpp
+++ CS107/exc/L/W05/Point3D/w05.cpp
@@ -13,17 +13,17 @@ int main() {
 	
 	srand(time(NULL));
 	int n = rng(3, 7);
-	vector<Point3D> vp3;
 	
 	cout << "Please enter " << n << " 3D points (Format: \"x, y, z\"):\n";
 	
+	vector<Point3D> vp3;
+	vp3.reserve(n);
 	vp3 = extractor3D(input3D(n));
 	
 	cout << "\n\nFound " << vp3.size() << " point";
 	(vp3.size() == 1) ? cout << ":\n" : cout << "s:\n";
 	printVector3D(vp3);
 	
-	cout << vp3[0].getX();
 	writeFile(dir_full, vp3);
 	
 	return 0;
