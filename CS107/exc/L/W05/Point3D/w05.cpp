0c8914daf336f80508504f4233579ffe1ffbd630
Đức Đào <duc.dao.431@gmail.com>
2022-06-01 15:27:13 +0700

    [add] 107.L.05
    Minor bugs in src/points.cpp

---
+++ CS107/exc/L/W05/Point3D/w05.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "src/io.h"
+#include "src/point.h"
+#include "src/rng.h"
+
+using std::cout;
+
+
+int main() {
+	string const dir_full = "/home/dmd/Documents/University/CS107/exc/L/W05/Point3D/point3d.txt";
+	string const dir = "point3d.txt";
+	
+	srand(time(NULL));
+	int n = rng(3, 7);
+	vector<Point3D> vp3;
+	
+	cout << "Please enter " << n << " 3D points (Format: \"x, y, z\"):\n";
+	
+	vp3 = extractor3D(input3D(n));
+	
+	cout << "\n\nFound " << vp3.size() << " point";
+	(vp3.size() == 1) ? cout << ":\n" : cout << "s:\n";
+	printVector3D(vp3);
+	
+	cout << vp3[0].getX();
+	writeFile(dir_full, vp3);
+	
+	return 0;
+}
