03fde87e5469dd2e16e053f906a0f668a9921e6a
Đức Đào <duc.dao.431@gmail.com>
2022-09-15 20:37:01 +0700

    202.L.01-02.01
    Beautify
    
    
    Former-commit-id: 4faf4e273a3542835a11d1b968a4e1ab8e5ad428

--- CS202/L/w01/PersonalFinanceAdvisor/Program.cs
+++ CS202/L/w01/PersonalFinanceAdvisor/Program.cs
@@ -8,27 +8,27 @@ namespace PersonalFinanceAdvisor
 	{
 		static Tuple<float, float, float, float, float, float> JARS(float income)
 		{
-			float[] distribution = new float[6] { 0.55f * income,		// Neccessities
-											 	  0.1f * income,        // Long-term
-											 	  0.1f * income,        // Play
-											 	  0.1f * income,        // Education
-											 	  0.1f * income,        // Financial
-											 	  0.05f * income };		// Give
+			float[] distro = new float[6] { (0.55f * income),	// Neccessities
+											(0.1f * income),	// Long-term
+											(0.1f * income),    // Play
+											(0.1f * income),    // Education
+											(0.1f * income),    // Financial
+											(0.05f * income) };	// Give
 
-			Tuple<float, float, float, float, float, float> jars
-				= new Tuple<float, float, float, float, float, float>(distribution[0], distribution[1], distribution[2], distribution[3], distribution[4], distribution[5]);
+			Tuple<float, float, float, float, float, float> jars = new( distro[0], distro[1], distro[2],
+																		distro[3], distro[4], distro[5] );
 			
 			return jars;
 		}
 		
-		static void Main(string[] args)
+		static void Main()
 		{
 			// Ask for user's monthly income
-			float income = new float();
+			float income = 0f;
 			do
 			{
 				Console.WriteLine("Enter your monthly income:");
-				string usrInp = new string(Console.ReadLine());
+				string usrInp = new(Console.ReadLine());
 
 				// Parse input to number
 				income = float.Parse(usrInp, CultureInfo.InvariantCulture.NumberFormat);
@@ -40,12 +40,12 @@ namespace PersonalFinanceAdvisor
 			Console.WriteLine("Your JARS are:");
 
 			// https://www.csharp-examples.net/string-format-double/
-			Console.WriteLine("Neccessities: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item1));
-			Console.WriteLine("Long-term: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item2));
-			Console.WriteLine("Play: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item3));
-			Console.WriteLine("Education: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item4));
-			Console.WriteLine("Financial: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item5));
-			Console.WriteLine("Give: ".PadLeft(15) + string.Format("{0:0,0.###}", jars.Item6));
+			Console.WriteLine("Neccessities: ".PadLeft(15)	+ string.Format("{0:0,0.###}", jars.Item1));
+			Console.WriteLine("Long-term: ".PadLeft(15)		+ string.Format("{0:0,0.###}", jars.Item2));
+			Console.WriteLine("Play: ".PadLeft(15)			+ string.Format("{0:0,0.###}", jars.Item3));
+			Console.WriteLine("Education: ".PadLeft(15)		+ string.Format("{0:0,0.###}", jars.Item4));
+			Console.WriteLine("Financial: ".PadLeft(15)		+ string.Format("{0:0,0.###}", jars.Item5));
+			Console.WriteLine("Give: ".PadLeft(15)			+ string.Format("{0:0,0.###}", jars.Item6));
 		}
 	}
 }
