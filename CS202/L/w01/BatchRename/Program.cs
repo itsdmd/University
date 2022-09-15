3c2aabf84a453b5a9e5fb51864d8f305d7fa3538
Đức Đào <duc.dao.431@gmail.com>
2022-09-15 15:10:24 +0700

    [add] 202.L.01-02
    Fully functional
    
    
    Former-commit-id: 2274dd2df9535418971aad7241fa23975b147ac3

---
+++ CS202/L/w01/BatchRename/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// 1. Replace some special characters into space: -  _
+//For example: "jack-forsel-cv.pdf" ⇒ Should be "jack forsel cv.pdf" 
+
+//2. The first letter of the word in the original filename should be in uppercase.
+//For example: "jack  forsel  cv.pdf" ⇒ Should be "Jack  Forsel  Cv.pdf"
+
+//3. Make sure there are no more than one spaces between words
+//For example: "jack      forsel  cv.pdf" ⇒  Should be "jack forsel cv.pdf"
+
+//4. Add a prefix of the current date to the file, in the format of YYMMDD
+//For example: "jack forsel  cv.pdf" ⇒ Should be "210924 jack forsel cv.pdf"
+
+namespace BatchRename
+{
+	class Program
+	{
+		static string Formatter(string inp){
+			// Split file name into words
+			char[] delimiterChars = { ' ', '-', '_' };
+			string[] words = inp.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+			// Capitalize first character of each word
+			for (int i = 0; i < words.Length; i++)
+			{
+				words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1);
+			}
+
+			// Get the current date
+			DateTime date = DateTime.Now;
+
+			// Stitching
+			StringBuilder builder = new StringBuilder(date.ToString("yyMMdd"));
+			foreach (string word in words)
+			{
+				builder.Append(' ');
+				builder.Append(word);
+			}
+
+			return builder.ToString();
+		}
+
+		static void Renamer(List<string> inp)
+		{
+			foreach(string name in inp)
+			{
+				Console.WriteLine(Formatter(name));
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			var filenames = new List<string>() {
+				"Ernesto Lowe.pdf",
+				"Calvin-hopkins.pdf",
+				"Brandon    Mullins.pdf",
+				"Jon Wood.pdf",
+				"marianne-owers.pdf",
+				"Luis--Chavez.pdf",
+				"Cecil Logan.pdf",
+				"Saul_kennedy.pdf",
+				"Raul___Thompson___CV.pdf",
+				"Juana--Wagner.pdf",
+				"sophie_chapman.pdf",
+				"Donna___Mason.pdf",
+				"garry-roy.pdf",
+				"Drew sparks.pdf",
+				"Jeffrey norris.pdf",
+				"Dwayne_townsend.pdf",
+				"Violet-Garza.pdf",
+				"Oscar----Ellis.pdf",
+				"jeremiah wwest.pdf",
+				"emma    Padilla.pdf",
+			};
+
+			Console.WriteLine("File names after renaming:");
+			Renamer(filenames);
+		}
+	}
+}
