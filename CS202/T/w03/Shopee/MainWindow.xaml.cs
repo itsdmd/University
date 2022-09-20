32bb4ea817280d68eb5cd0b8722976a6eeb28807
Đức Đào <duc.dao.431@gmail.com>
2022-09-20 20:52:17 +0100

    [add] 202.T.03-01
    Shopee

---
+++ CS202/T/w03/Shopee/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shopee
+{
+	/// <summary>
+	/// Interaction logic for Shopee.xaml
+	/// </summary>
+	public partial class Shopee : Window
+	{
+		public void SetFlashSale()
+		{
+			fst1.Text = "109.000";
+			fst2.Text = "150.000";
+			fst3.Text = "115.000";
+			fst4.Text = "169.000";
+			fst5.Text = "217.000";
+			fst6.Text = "91.000";
+		}
+
+		public Shopee()
+		{
+			InitializeComponent();
+			SetFlashSale();
+		}
+	}
+}
