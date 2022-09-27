2dc8546091e46dd2a27c419e7a53618ac0f61be5
Đức Đào <duc.dao.431@gmail.com>
2022-09-27 19:13:36 +0100

    [add] 202.T.03-02
    Submission, fully functional
    
    
    Former-commit-id: 426143cad6f4d0430a1cd8b2766825f410f575b8

---
+++ CS202/T/w03/BookBinding/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookBinding
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		class Book
+		{
+			public string Name { get; set; }
+			public string Author { get; set; }
+			public int PubYear { get; set; }
+			public string CoverPath { get; set; }
+		}
+
+
+		static Book _book = new Book()
+		{
+			Name = "Problems in General Physics",
+			Author = "I. F. Irodov",
+			PubYear = 2011,
+			CoverPath = "res/cover.jpg"
+		};
+
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			this.DataContext = _book;
+		}
+	}
+}
