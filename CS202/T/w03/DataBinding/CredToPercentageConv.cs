20d3d4065ca514f6cb22d5e1e68009e8b9124405
Đức Đào <duc.dao.431@gmail.com>
2022-09-27 09:08:50 +0100

    [add] 202.T.03-01
    Demo, fully functional
    
    
    Former-commit-id: 74ea70c18e3a0058f2818ab5081832c12031dcda

---
+++ CS202/T/w03/DataBinding/CredToPercentageConv.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataBinding
+{
+	class CredToPercentageConv : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int credits = (int)value;
+			double percentage = (double)credits * 100 / 140;
+			return ($"{percentage:0.00}%");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
