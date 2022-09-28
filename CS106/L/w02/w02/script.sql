4f908f37e4f2a3c9acc4b4ebdbb52a95a3e541f0
Đức Đào <duc.dao.431@gmail.com>
2022-09-28 20:17:34 +0100

    202.L.02.02
    Fully functional
    
    
    Former-commit-id: 1586b10aa7fc0c03b3b288c46349f087ce0f3488

--- CS106/L/w02/w02/script.sql
+++ CS106/L/w02/w02/script.sql
@@ -4,7 +4,9 @@ USE QLGV
 GO
 
 
--- Create Tables
+----- GiaoVien + Khoa + BoMon -----
+
+-- Create tables
 
 CREATE TABLE GiaoVien
 (
@@ -47,37 +49,52 @@ CREATE TABLE BoMon
 )
 
 
--- Create Foreign Keys
+-- Create foreign keys
 
 ALTER TABLE GiaoVien
-	ADD CONSTRAINT FK_MaGV_GVQLCM
+	ADD CONSTRAINT FK_GiaoVien__GVQLCM
 	FOREIGN KEY(GVQLCM)
 	REFERENCES GiaoVien(MaGV)
 	
 ALTER TABLE GiaoVien
-	ADD CONSTRAINT FK_MaBM_BoMon
+	ADD CONSTRAINT FK_GiaoVien__MaBM
 	FOREIGN KEY(MaBM)
 	REFERENCES BoMon(MaBM)
 
-
 ALTER TABLE Khoa
-	ADD CONSTRAINT FK_TruongKhoa_GiaoVien
+	ADD CONSTRAINT FK_Khoa__MaGV
 	FOREIGN KEY(TruongKhoa)
 	REFERENCES GiaoVien(MaGV)
 
-
 ALTER TABLE BoMon
-	ADD CONSTRAINT FK_TruongBoMon_GiaoVien
+	ADD CONSTRAINT FK_BoMon__MaGV
 	FOREIGN KEY(TruongBoMon)
 	REFERENCES GiaoVien(MaGV)
 
 ALTER TABLE BoMon
-	ADD CONSTRAINT FK_MaKhoa_Khoa
+	ADD CONSTRAINT FK_BoMon__MaKhoa
 	FOREIGN KEY(MaKhoa)
 	REFERENCES Khoa(MaKhoa)
+	
 
+-- Data input
 
--- Data Input
+INSERT INTO GiaoVien VALUES('001', N'Nguyễn Hoài An', 2000, N'Nam', '1990-02-15', N'25/3 Lạc Long Quân, Q.10, TP HCM', NULL, NULL)
+INSERT INTO GiaoVien VALUES('002', N'Trần Trà Hương', 2500, N'Nữ', '1960-06-20', N'125 Trần Hưng Đạo, Q.1, TP HCM', NULL, NULL)
+INSERT INTO GiaoVien VALUES('003', N'Nguyễn Ngọc Ánh', 2200, N'Nữ', '1975-05-11', N'12/21 Võ Văn Ngân, Thủ Đức, TP HCM', '002', NULL)
+INSERT INTO GiaoVien VALUES('004', N'Trương Nam Sơn', 2300, N'Nam', '1959-06-20', N'215 Lý Thường Kiệt, TP Biên Hòa', NULL, NULL)
+INSERT INTO GiaoVien VALUES('005', N'Lý Hoàng Hà', 2500, N'Nam', '1954-10-23', N'22/5 Nguyễn Xí, Q. Bình Thạnh, TP HCM', NULL, NULL)
+INSERT INTO GiaoVien VALUES('006', N'Trần Bạch Tuyết', 1500, N'Nữ', '1980-05-20', N'127 Hùng Vương, TP Mỹ Tho', '004', NULL)
+INSERT INTO GiaoVien VALUES('007', N'Nguyễn An Trung', 1800, N'Nam', '1976-06-05', N'234 3/2, TP Biên Hòa', NULL, NULL)
+INSERT INTO GiaoVien VALUES('008', N'Trần Trung Hiếu', 1800, N'Nam', '1977-08-06', N'22/11 Lý Thường Kiệt, TP Mỹ Tho', '007', NULL)
+INSERT INTO GiaoVien VALUES('009', N'Trần Hoàng Nam', 2000, N'Nam', '1975-11-22', N'234 Trấn Não, An Phú, TP HCM', '001', NULL)
+INSERT INTO GiaoVien VALUES('010', N'Phạm Nam Thand', 1500, N'Nam', '1980-12-22', N'221 Hùng Vương, Q.5, TP HCM', '007', NULL)
+
+INSERT INTO Khoa VALUES('CNTT', 'Công nghệ thông tin', 1995, 'B11', '0838123456', '002', '2005-02-20')
+INSERT INTO Khoa VALUES('HH', 'Hóa học', 1980, 'B41', '0838456456', '007', '2001-10-15')
+INSERT INTO Khoa VALUES('SH', 'Sinh học', 1980, 'B31', '0838454545', '004', '2000-10-11')
+INSERT INTO Khoa VALUES('VL', 'Vật lý', 1976, 'B21', '0838223223', '005', '2003-09-18')
+
 INSERT INTO BoMon VALUES('CNTT', N'Công nghệ tri thức', 'B15', '0838126126', NULL, 'CNTT', NULL)
 INSERT INTO BoMon VALUES('HHC', N'Hóa hữu cơ', 'B44', '838222222', NULL, 'HH', NULL)
 INSERT INTO BoMon VALUES('HL', N'Hóa lý', 'B42', '0838878787', NULL, 'HH', NULL)
@@ -89,13 +106,189 @@ INSERT INTO BoMon VALUES('VLĐT', N'Vật lý điện tử', 'B23', '0838234234'
 INSERT INTO BoMon VALUES('VLƯD', N'Vật lý ứng dụng', 'B24', '0838454545', '005', 'VL', '2006-02-18')
 INSERT INTO BoMon VALUES('VS', N'Vi sinh', 'B32', '0838909090', '004', 'SH', '2007-01-01')
 
-INSERT INTO GiaoVien VALUES('001', N'Nguyễn Hoài An', 2000, N'Nam', '1990-02-15', N'25/3 Lạc Long Quân, Q.10, TP HCM', NULL, 'MMT')
-INSERT INTO GiaoVien VALUES('002', N'Trần Trà Hương', 2500, N'Nữ', '1960-06-20', N'125 Trần Hưng Đạo, Q.1, TP HCM', NULL, 'HTTT')
-INSERT INTO GiaoVien VALUES('003', N'Nguyễn Ngọc Ánh', 2200, N'Nữ', '1975-05-11', N'12/21 Võ Văn Ngân, Thủ Đức, TP HCM', '002', 'HTTT')
-INSERT INTO GiaoVien VALUES('004', N'Trương Nam Sơn', 2300, N'Nam', '1959-06-20', N'215 Lý Thường Kiệt, TP Biên Hòa', NULL, 'VS')
-INSERT INTO GiaoVien VALUES('005', N'Lý Hoàng Hà', 2500, N'Nam', '1954-10-23', N'22/5 Nguyễn Xí, Q. Bình Thạnh, TP HCM', NULL, 'VLĐT')
-INSERT INTO GiaoVien VALUES('006', N'Trần Bạch Tuyết', 1500, N'Nữ', '1980-05-20', N'127 Hùng Vương, TP Mỹ Tho', '004', 'VS')
-INSERT INTO GiaoVien VALUES('007', N'Nguyễn An Trung', 1800, N'Nam', '1976-06-05', N'234 3/2, TP Biên Hòa', NULL, 'HPT')
-INSERT INTO GiaoVien VALUES('008', N'Trần Trung Hiếu', 1800, N'Nam', '1977-08-06', N'22/11 Lý Thường Kiệt, TP Mỹ Tho', '007', 'HPT')
-INSERT INTO GiaoVien VALUES('009', N'Trần Hoàng Nam', 2000, N'Nam', '1975-11-22', N'234 Trấn Não, An Phú, TP HCM', '001', 'MMT')
-INSERT INTO GiaoVien VALUES('010', N'Phạm Nam Thand', 1500, N'Nam', '1980-12-22', N'221 Hùng Vương, Q.5, TP HCM', '007', 'HPT')
+UPDATE GiaoVien SET MaBM = 'MMT' WHERE MaGV = '001'
+UPDATE GiaoVien SET MaBM = 'HTTT' WHERE MaGV = '002'
+UPDATE GiaoVien SET MaBM = 'HTTT' WHERE MaGV = '003'
+UPDATE GiaoVien SET MaBM = 'VS' WHERE MaGV = '004'
+UPDATE GiaoVien SET MaBM = 'VLĐT' WHERE MaGV = '005'
+UPDATE GiaoVien SET MaBM = 'VS' WHERE MaGV = '006'
+UPDATE GiaoVien SET MaBM = 'HPT' WHERE MaGV = '007'
+UPDATE GiaoVien SET MaBM = 'HPT' WHERE MaGV = '008'
+UPDATE GiaoVien SET MaBM = 'MMT' WHERE MaGV = '009'
+UPDATE GiaoVien SET MaBM = 'HPT' WHERE MaGV = '010'
+
+
+----- GV_DT + NguoiThan -----
+
+-- Create tables
+CREATE TABLE GV_DT
+(
+	MaGV CHAR(3),
+	DienThoai VARCHAR(11),
+
+	CONSTRAINT PK_GV_DT PRIMARY KEY(MaGV, DienThoai)
+)
+
+CREATE TABLE NguoiThan
+(
+	MaGV CHAR(3),
+	Ten VARCHAR(50),
+	NgSinh DATE,
+	Phai VARCHAR(3),
+
+	CONSTRAINT PK_NguoiThan PRIMARY KEY(MaGV, Ten)
+)
+
+
+-- Create foreign keys
+
+ALTER TABLE GV_DT
+	ADD CONSTRAINT FK_GV_DT__MaGV
+	FOREIGN KEY(MaGV)
+	REFERENCES GiaoVien(MaGV)
+
+ALTER TABLE NguoiThan
+	ADD CONSTRAINT FK_NguoiThan__MaGV
+	FOREIGN KEY(MaGV)
+	REFERENCES GiaoVien(MaGV)
+
+
+-- Insert data
+
+INSERT INTO GV_DT VALUES('001', '0838912112')
+INSERT INTO GV_DT VALUES('001', '0903123123')
+INSERT INTO GV_DT VALUES('002', '0913453535')
+INSERT INTO GV_DT VALUES('003', '0838121212')
+INSERT INTO GV_DT VALUES('003', '0903656565')
+INSERT INTO GV_DT VALUES('003', '0937125125')
+INSERT INTO GV_DT VALUES('006', '0937888888')
+INSERT INTO GV_DT VALUES('008', '0653717171')
+INSERT INTO GV_DT VALUES('008', '0913232323')
+
+INSERT INTO NguoiThan VALUES('001', N'Hùng', '1990-01-14', N'Nam')
+INSERT INTO NguoiThan VALUES('001', N'Thủy', '1994-12-08', N'Nữ')
+INSERT INTO NguoiThan VALUES('003', N'Hà', '1998-09-03', N'Nữ')
+INSERT INTO NguoiThan VALUES('003', N'Thu', '1998-09-03', N'Nữ')
+INSERT INTO NguoiThan VALUES('007', N'Mai', '2003-03-26', N'Nữ')
+INSERT INTO NguoiThan VALUES('007', N'Vy', '2000-02-14', N'Nữ')
+INSERT INTO NguoiThan VALUES('008', N'Nam', '1991-05-06', N'Nam')
+INSERT INTO NguoiThan VALUES('009', N'An', '1996-08-19', N'Nam')
+INSERT INTO NguoiThan VALUES('010', N'Nguyệt', '2006-01-14', N'Nữ')
+
+
+----- ChuDe + DeTai + CongViec + ThamGiaDT  -----
+
+-- Create Tables
+
+CREATE TABLE ChuDe
+(
+	MaCD NCHAR(4),
+	TenCD NVARCHAR(50),
+
+	CONSTRAINT PK_ChuDe PRIMARY KEY(MaCD)
+)
+
+CREATE TABLE DeTai
+(
+	MaDT CHAR(3),
+	TenDT NVARCHAR(50),
+	CapQL NVARCHAR(10),
+	KinhPhi FLOAT,
+	NgayBD DATE,
+	NgayKT DATE,
+	MaCD NCHAR(4),
+	GVCNDT CHAR(3),
+
+	CONSTRAINT PK_DeTai PRIMARY KEY(MaDT),
+)
+
+CREATE TABLE CongViec
+(
+	MaDT CHAR(3),
+	SoTT INT,
+	TenCV NVARCHAR(50),
+	NgayBD DATE,
+	NgayKT DATE,
+
+	CONSTRAINT PK_CongViec PRIMARY KEY(MaDT, SoTT),
+)
+
+CREATE TABLE ThamGiaDT
+(
+	MaGV CHAR(3),
+	MaDT CHAR(3),
+	STT INT,
+	PhuCap FLOAT,
+	KetQua NVARCHAR(4),
+
+	CONSTRAINT PK_ThamGiaDT PRIMARY KEY(MaGV, MaDT, STT),
+)
+
+
+-- Create foreign keys
+
+ALTER TABLE DeTai
+	ADD CONSTRAINT FK_DeTai__MaCD
+	FOREIGN KEY(MaCD)
+	REFERENCES ChuDe(MaCD)
+
+ALTER TABLE DeTai
+	ADD CONSTRAINT FK_DeTai__MaGV
+	FOREIGN KEY(GVCNDT)
+	REFERENCES GiaoVien(MaGV)
+
+ALTER TABLE CongViec
+	ADD CONSTRAINT FK_CongViec__MaDT
+	FOREIGN KEY(MaDT)
+	REFERENCES DeTai(MaDT)
+
+ALTER TABLE ThamGiaDT
+	ADD CONSTRAINT FK_ThamGiaDT__MaGV
+	FOREIGN KEY(MaGV)
+	REFERENCES GiaoVien(MaGV)
+
+ALTER TABLE ThamGiaDT
+	ADD CONSTRAINT FK_ThamGiaDT__MaDT_SoTT
+	FOREIGN KEY(MaDT, STT)
+	REFERENCES CongViec(MaDT, SoTT)
+
+
+-- Insert data
+
+INSERT INTO ChuDe VALUES('NCPT', N'Nghiên cứu phát triển')
+INSERT INTO ChuDe VALUES('QLGD', N'Quản lý giáo dục')
+INSERT INTO ChuDe VALUES('UDCN', N'Ứng dụng công nghệ')
+
+INSERT INTO DeTai VALUES('001', N'HTTT quản lý các trường ĐH', N'ĐHQG', 20, '2007-10-20', '2008-10-20', 'QLGD', '002')
+INSERT INTO DeTai VALUES('002', N'HTTT quản lý giáo vụ cho một khoa', N'Trường', 20, '2000-10-12', '2001-10-12', 'QLGD', '002')
+INSERT INTO DeTai VALUES('003', N'Nghiên cứu chế tạo sợi Nanô Platin', N'ĐHQG', 300, '2008-05-15', '2010-05-15', 'NCPT', '005')
+INSERT INTO DeTai VALUES('004', N'Tạo vật liệu sinh học bằng màng ối người', N'Nhà nước', 100, '2007-01-01', '2009-12-31', 'NCPT', '004')
+INSERT INTO DeTai VALUES('005', N'Ứng dụng hóa học xanh', N'Trường', 200, '2003-10-10', '2004-12-10', 'UDCN', '007')
+INSERT INTO DeTai VALUES('006', N'Nghiên cứu tế bào gốc', N'Nhà nước', 4000, '2006-10-20', '2009-10-20', 'NCPT', '004')
+INSERT INTO DeTai VALUES('007', N'HTTT quản lý thư viện ở các trường ĐH', N'Trường', 20, '2009-05-10', '2010-05-10', 'QLGD', '001')
+
+INSERT INTO CongViec VALUES('001', 1, N'Khởi tạo và Lập kế hoạch', '2007-10-20', '2008-12-20')
+INSERT INTO CongViec VALUES('001', 2, N'Xác định yêu cầu', '2008-12-21', '2008-03-21')
+INSERT INTO CongViec VALUES('001', 3, N'Phân tích hệ thống', '2008-03-22', '2008-05-22')
+INSERT INTO CongViec VALUES('001', 4, N'Thiết kế hệ thống', '2008-05-23', '2008-06-23')
+INSERT INTO CongViec VALUES('001', 5, N'Cài đặt thử nghiệm', '2008-06-24', '2008-10-20')
+INSERT INTO CongViec VALUES('002', 1, N'Khởi tạo và Lập kế hoạch', '2009-05-10', '2009-07-10')
+INSERT INTO CongViec VALUES('002', 2, N'Xác định yêu cầu', '2009-07-11', '2009-10-11')
+INSERT INTO CongViec VALUES('002', 3, N'Phân tích hệ thống', '2009-10-12', '2009-12-20')
+INSERT INTO CongViec VALUES('002', 4, N'Thiết kế hệ thống', '2009-12-21', '2010-03-22')
+INSERT INTO CongViec VALUES('002', 5, N'Cài đặt thử nghiệm', '2010-03-23', '2010-05-10')
+INSERT INTO CongViec VALUES('006', 1, N'Lấy mẫu', '2006-10-20', '2007-02-20')
+INSERT INTO CongViec VALUES('006', 2, N'Nuôi cấy', '2007-02-21', '2008-08-21')
+
+INSERT INTO ThamGiaDT VALUES('001', '002', 1, 0, 'NULL')
+INSERT INTO ThamGiaDT VALUES('001', '002', 2, 2, 'NULL')
+INSERT INTO ThamGiaDT VALUES('002', '001', 4, 2, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('003', '001', 1, 1, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('003', '001', 2, 0, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('003', '001', 4, 1, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('003', '002', 2, 0, 'NULL')
+INSERT INTO ThamGiaDT VALUES('004', '006', 1, 0, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('004', '006', 2, 1, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('006', '006', 2, 1.5, N'Đạt')
+INSERT INTO ThamGiaDT VALUES('009', '002', 3, 0.5, 'NULL')
+INSERT INTO ThamGiaDT VALUES('009', '002', 4, 1.5, 'NULL')
