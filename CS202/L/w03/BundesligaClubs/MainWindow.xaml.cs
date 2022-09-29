0a324957dcc34cb7072f5c77ead6afa3d43fce2a
Đức Đào <duc.dao.431@gmail.com>
2022-09-29 14:19:02 +0100

    [add] 202.L.03-02
    Fully functional
    
    
    Former-commit-id: 0df9a30f47f74123515592b5af66330465f96752

---
+++ CS202/L/w03/BundesligaClubs/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace BundesligaClubs
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		string[] resFiles;
+		readonly string resDir = $"{AppDomain.CurrentDomain.BaseDirectory}res";
+
+		public string[] GetImageFiles()
+		{
+			resFiles = Directory.GetFiles(resDir);
+
+			return resFiles;
+		}
+
+		class Entry
+		{
+			public string Logo { get; set; }
+			public string Name { get; set; }
+		}
+
+		static Entry _entry = new Entry();
+
+        public Tuple<string,string> NextEntry()
+        {
+            Random rng = new Random();
+			
+            string path = resFiles[rng.Next(resFiles.Length)];
+            string text = path.Substring(resDir.Length + 1)    // Remove ".../res/"
+                              .Split('.')[0]                    // Remove ".png"
+                              .Replace('_', ' ');
+
+			return new Tuple<string, string>(path, text);
+        }
+
+		public void DisplayNewEntry()
+		{
+			Tuple<string, string> entry = NextEntry();
+
+			_entry = new Entry
+			{
+				Logo = entry.Item1,
+				Name = entry.Item2
+			};
+		}
+        
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+			DisplayNewEntry();
+			this.DataContext = _entry;
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			GetImageFiles();
+			DisplayNewEntry();
+			this.DataContext = _entry;
+		}
+	}
+}
