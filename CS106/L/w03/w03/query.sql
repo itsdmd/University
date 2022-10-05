e877f1f26d0a69325303c74c9295352b4f708797
Đức Đào <duc.dao.431@gmail.com>
2022-10-05 08:38:15 +0100

    [add] 106.L.03
    WIP

---
+++ CS106/L/w03/w03/query.sql
@@ -0,0 +1,38 @@
+USE QLGV
+GO
+
+-- 1. Luong GV nu
+SELECT Luong
+FROM GiaoVien
+WHERE Phai = N'Nữ'
+
+-- 3. MaGV cua (TenGV bat dau bang 'Nguyen' & Luong > 2000) | GVTBM nhan chuc sau 1995
+SELECT MaGV
+FROM GiaoVien
+WHERE (TenGV LIKE N'Nguyễn%') AND (Luong > 2000)
+UNION
+SELECT TruongBoMon
+FROM BoMon
+WHERE (YEAR(NgayNhanChuc) > 1995)
+
+-- 5. BoMon + GVTBM
+SELECT *
+FROM BoMon
+JOIN GiaoVien
+ON BoMon.TruongBoMon = GiaoVien.MaGV
+
+-- 7. DeTai + GVCNDT
+SELECT *
+FROM DeTai
+JOIN GiaoVien
+ON DeTai.GVCNDT = GiaoVien.MaGV
+
+-- 9. GiaoVien (TenBoMon = 'Vi sinh') & (MaDT = '006')
+SELECT *
+FROM GiaoVien
+WHERE MaBM = (SELECT MaBM
+              FROM BoMon
+              WHERE TenBoMon = N'Vi sinh')
+    AND MaGV IN (SELECT GVCNDT
+                 FROM DeTai
+                 WHERE MaDT = '006')
