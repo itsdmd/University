e89e6aeccda27c8742871a857dca39b1eb6e1c46
Đức Đào <duc.dao.431@gmail.com>
2022-10-05 09:34:55 +0100

    106.L.03.02
    WIP
    
    
    Former-commit-id: 701a6a3230e96e98c2d5b679f807bced7dfc6361

--- CS106/L/w03/w03/query.sql
+++ CS106/L/w03/w03/query.sql
@@ -6,6 +6,10 @@ SELECT Luong
 FROM GiaoVien
 WHERE Phai = N'Nữ'
 
+-- 2. TenGV + Luong +10%
+SELECT TenGV, Luong*1.1 as Luong10
+FROM GiaoVien
+
 -- 3. MaGV cua (TenGV bat dau bang 'Nguyen' & Luong > 2000) | GVTBM nhan chuc sau 1995
 SELECT MaGV
 FROM GiaoVien
@@ -15,24 +19,58 @@ SELECT TruongBoMon
 FROM BoMon
 WHERE (YEAR(NgayNhanChuc) > 1995)
 
+-- 4. TenGV co (TenKhoa = N'Công nghệ thông tin')
+SELECT TenGV
+FROM GiaoVien
+    LEFT JOIN BoMon ON BoMon.MaBM = GiaoVien.MaBM
+    LEFT JOIN Khoa ON Khoa.MaKhoa = BoMon.MaKhoa
+WHERE Khoa.TenKhoa = N'Công nghệ thông tin'
+
 -- 5. BoMon + GVTBM
 SELECT *
 FROM BoMon
-LEFT JOIN GiaoVien
+JOIN GiaoVien
 ON BoMon.TruongBoMon = GiaoVien.MaGV
+
+-- 6. GiaoVien + BoMon
+SELECT *
+FROM GiaoVien
+    LEFT JOIN BoMon
+ON GiaoVien.MaBM = BoMon.MaBM
 
 -- 7. DeTai + GVCNDT
 SELECT *
 FROM DeTai
-LEFT JOIN GiaoVien
+    LEFT JOIN GiaoVien
 ON DeTai.GVCNDT = GiaoVien.MaGV
+
+-- 8. Khoa + TruongKhoa
+SELECT *
+FROM Khoa
+    LEFT JOIN GiaoVien
+ON Khoa.TruongKhoa = GiaoVien.MaGV
 
 -- 9. GiaoVien (TenBoMon = 'Vi sinh') & (MaDT = '006')
 SELECT *
 FROM GiaoVien
-WHERE MaBM = (SELECT MaBM
-              FROM BoMon
-              WHERE TenBoMon = N'Vi sinh')
-    AND MaGV IN (SELECT GVCNDT
-                 FROM DeTai
-                 WHERE MaDT = '006')
+    LEFT JOIN BoMon ON BoMon.MaBM = GiaoVien.MaBM
+    LEFT JOIN DeTai ON DeTai.GVCNDT = GiaoVien.MaGV
+WHERE (BoMon.TenBoMon = N'Vi sinh') AND (DeTai.MaDT = '006')
+
+-- 10. DeTai (CapQL = 'Thành phố'): TenDT, ChuDe, GVCNDT(TenGV + NgSinh + DiaChi)
+SELECT TenDT, ChuDe.MaCD, ChuDe.TenCD, TenGV, NgSinh, DiaChi
+FROM DeTai
+    LEFT JOIN ChuDe ON DeTai.MaCD = ChuDe.MaCD
+    LEFT JOIN GiaoVien ON DeTai.GVCNDT = GiaoVien.MaGV
+WHERE CapQL = N'Thành phố'
+
+-- 11. TenGV + GVQLCM(TenGV)
+SELECT GV.TenGV, GVQL.TenGV AS TenGVQLCM
+FROM GiaoVien AS GV
+    LEFT JOIN GiaoVien AS GVQL ON GV.GVQLCM = GVQL.MaGV
+
+-- 12. TenGV co TenGVQLCM = 'Nguyễn Thanh Tùng'
+SELECT GV.TenGV
+FROM GiaoVien AS GV
+LEFT JOIN GiaoVien AS GVQLCM ON GV.GVQLCM = GVQLCM.MaGV
+WHERE GVQLCM.TenGV = N'Nguyễn Thanh Tùng'
