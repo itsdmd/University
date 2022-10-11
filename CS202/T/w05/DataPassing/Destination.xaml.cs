452107615972b5600d418af3af664fa0bdcd16b3
Đức Đào <duc.dao.431@gmail.com>
2022-10-11 14:18:43 +0100

    [add] 202.T.05
    DataPassing, fully functional

---
+++ CS202/T/w05/DataPassing/Destination.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataPassing
+{
+	public partial class Destination : Window
+	{
+		// Hold modified data from the textbox
+		public Student ReturnData { get; set; }
+
+		public Destination(Student _sv)
+		{
+			InitializeComponent();
+
+			ReturnData = (Student)_sv.Clone();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			DataContext = ReturnData;
+		}
+
+		private void okButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Close the window and return the modified data
+			DialogResult = true;
+		}
+	}
+}
