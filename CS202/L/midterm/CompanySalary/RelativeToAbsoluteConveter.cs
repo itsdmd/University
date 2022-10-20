f74fadb92cc6c4d8dbccc6f32c80fcf8b3db416a
Đức Đào <duc.dao.431@gmail.com>
2022-10-20 15:34:09 +0100

    [add] 202.L.midterm
    Partially functional

---
+++ CS202/L/midterm/CompanySalary/RelativeToAbsoluteConveter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CompanySalary
+{
+    public class RelativeToAbsoluteConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var folder = AppDomain.CurrentDomain.BaseDirectory;
+
+            string relativePath = (string)value;
+            string absolutePath = $"{folder}{relativePath}";
+
+            return absolutePath;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
