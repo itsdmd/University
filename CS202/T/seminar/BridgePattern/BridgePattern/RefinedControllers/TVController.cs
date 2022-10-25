2807f3e464710227acaf67c18f89a94d3e92ee57
Đức Đào <duc.dao.431@gmail.com>
2022-10-25 19:39:21 +0100

    202.T.w07.02
    Split classes into .cs files
    Add: BaseDevice
    Rename: BasicController > BaseController
    
    Former-commit-id: 7d15085afed0ab65aa92e4982f4a8a8fc2e69641

---
+++ CS202/T/seminar/BridgePattern/BridgePattern/RefinedControllers/TVController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+	internal class TVController : BaseController
+	{
+		public TVController(IDevice device) : base(device) { }
+
+		public override void ChannelUp()
+		{
+			if (_device.IsEnabled() == false)
+			{
+				_device.Log("Cannot change channel. Device is disabled.");
+				return;
+			}
+
+			if (_device.GetChannel() == _device.GetLimits()["CHN_MAX"])
+			{
+				_device.SetChannel(_device.GetLimits()["CHN_MIN"]);
+			}
+			else
+			{
+				_device.SetChannel(_device.GetChannel() + 1);
+			}
+		}
+
+		public override void ChannelDown()
+		{
+			if (_device.IsEnabled() == false)
+			{
+				_device.Log("Cannot change channel. Device is disabled.");
+				return;
+			}
+
+			if (_device.GetChannel() == _device.GetLimits()["CHN_MIN"])
+			{
+				_device.SetChannel(_device.GetLimits()["CHN_MAX"]);
+			}
+			else
+			{
+				_device.SetChannel(_device.GetChannel() - 1);
+			}
+		}
+
+		// Platform-specic methods
+		public void Mute()
+		{
+			if (_device.IsEnabled() == false)
+			{
+				_device.Log("Cannot change volume. Device is disabled.");
+				return;
+			}
+
+			_device.SetVolume(0);
+		}
+	}
+}
