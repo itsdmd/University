c8f0f02c717ce503e46fac8cc0fd450df38626aa
Đức Đào <duc.dao.431@gmail.com>
2022-11-08 21:50:21 +0000

    202.T.final
    Fully functional
    
    
    Former-commit-id: dd995653bf75904450e4d7d298223b5581e9d807

---
+++ CS202/T/FinalProject/FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Contract;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.ComponentModel;
+
+namespace FinalProject
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public ObservableCollection<object> _originals = new ObservableCollection<object>();
+		public ObservableCollection<object> _previews = new ObservableCollection<object>();
+		RuleFactory f = new RuleFactory();
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			filesListView.ItemsSource = _originals;
+			previewListView.ItemsSource = _previews;
+		}
+
+		private void selectRuleButton_Click(object sender, RoutedEventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+
+			if (dialog.ShowDialog() == true)
+			{
+				var info = new FileInfo(dialog.FileName);
+
+				// Display the rule file name to TextBox
+				ruleFileName.Text = info.Name;
+
+				f.RuleFilePath = info.FullName;
+
+				var converter = FindResource("converter")
+					as RawToRenamedConverter;
+				converter!.Factory = f;
+
+				if (_originals.Count > 0)
+				{
+					_previews.Clear();
+
+					foreach (dynamic item in _originals)
+					{
+						_previews.Add(item);
+					}
+				}
+			}
+		}
+
+		private void addFileButton_Click(object sender, RoutedEventArgs e)
+		{
+			var dialog = new OpenFileDialog();
+			
+			if (dialog.ShowDialog() == true)
+			{
+				// Skip if already added
+				if (_originals.Contains(dialog.FileName)) { return; }
+				
+				var info = new FileInfo(dialog.FileName);
+
+				var rawItem = new
+				{
+					FullPath = dialog.FileName,
+					FileName = info.Name
+				};
+
+				_originals.Add(rawItem);
+				_previews.Add(rawItem);
+			}
+		}
+
+		private void addFolderButton_Click(object sender, RoutedEventArgs e)
+		{
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+			
+			dialog.InitialDirectory = "C:";
+			dialog.IsFolderPicker = true;
+			
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+			{
+				// Add all files in the folder
+				var files = Directory.GetFiles(dialog.FileName);
+				
+				foreach (var file in files)
+				{
+					// Skip if already added
+					if (_originals.Contains(dialog.FileName)) { return; }
+					
+					var info = new FileInfo(file);
+
+					var rawItem = new
+					{
+						FullPath = file,
+						FileName = info.Name
+					};
+
+					_originals.Add(rawItem);
+					_previews.Add(rawItem);
+				}
+			}
+		}
+
+		private void renameButton_Click(object sender, RoutedEventArgs e)
+		{
+			foreach(dynamic item in _originals)
+			{
+				f.FileName = item.FileName;
+				
+				var info = new FileInfo(item.FullPath);
+				var folder = info.Directory;
+				
+				// If moveToSubDirCheckBox is checked, create a subdirectory called "renamed" and copy the files to it
+				if (moveToSubDirCheckBox.IsChecked == true)
+				{
+					try
+					{
+						var newPath = $"{folder}\\renamed\\{f.Parse()}";
+						File.Copy(item.FullPath, newPath);
+					}
+					catch
+					{
+						folder = Directory.CreateDirectory(folder.FullName + "\\renamed");
+						var newPath = $"{folder}\\{f.Parse()}";
+						File.Copy(item.FullPath, newPath);
+					}
+				}
+				else
+				{
+					var newPath = $"{folder}\\{f.Parse()}";
+					File.Move(item.FullPath, newPath);
+				}
+			}
+			
+			MessageBox.Show($"Renamed {_originals.Count} files");
+		}
+	}
+}
---
+++ CS202/T/FinalProject/FinalProject/RuleFactory.cs
@@ -0,0 +1,100 @@
+﻿using Contract;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace FinalProject
+{
+	public class DLLReader
+	{
+		public Dictionary<string, IRule> GetPluginDict()
+		{
+			string folder = AppDomain.CurrentDomain.BaseDirectory;
+			DirectoryInfo folderInfo = new DirectoryInfo(folder);
+			var dllFiles = folderInfo.GetFiles("*.dll");
+
+			Dictionary<string, IRule> dict = new Dictionary<string, IRule>();
+
+			foreach (var file in dllFiles)
+			{
+				var assembly = Assembly.LoadFrom(file.FullName);
+
+				var types = assembly.GetTypes();
+
+				foreach (var type in types)
+				{
+					if (type.IsClass
+						&& typeof(IRule).IsAssignableFrom(type))
+					{
+						dict.Add(type.Name.Replace("Rule", ""),
+									(IRule)Activator.CreateInstance(type));
+					}
+				}
+			}
+
+			return dict;
+		}
+	}
+
+	public class RuleFactory
+	{
+		private static string _ruleFile = new string("");
+		private static string _fileName = new string("");
+
+		public string RuleFilePath { set { _ruleFile = value; } }
+		public string FileName { set { _fileName = value; } }
+
+		// Read rule file
+		static List<string> RuleReader()
+		{
+			try
+			{
+				return (new List<string>(File.ReadAllLines(_ruleFile)));
+			}
+			catch (Exception e)
+			{
+				MessageBox.Show("Rule file not found", e.ToString());
+				return null;
+			}
+		}
+
+		// Apply the rules
+		public string Parse()
+		{
+			if (_ruleFile == "")
+			{
+				return _fileName;
+			}
+
+			List<string> rules = RuleReader();
+			string renamed = _fileName;
+
+			Dictionary<string, IRule> rulePrototypes = new DLLReader().GetPluginDict();
+
+			foreach (string r in rules)
+			{
+				string ruleName = r.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
+				if (ruleName == null || ruleName == "") { continue; }
+
+				IRule irule = null;
+
+				try
+				{
+					irule = rulePrototypes[ruleName];
+				}
+				catch (KeyNotFoundException)
+				{
+					MessageBox.Show("Rule " + ruleName + " not found!");
+					continue;
+				}
+
+				irule.Parse = r;
+				renamed = irule.Rename(renamed);
+			}
+			
+			return renamed;
+		}
+	}
+}
