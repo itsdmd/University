34b500c2d4ffdc7bfe4bad19e2468375ce1a93c9
Đức Đào <duc.dao.431@gmail.com>
2022-11-16 07:42:56 +0000

    [add] 202.T.final
    Fully functional
    
    
    Former-commit-id: d7ea42f2e0f69f9d6b847cf30d0e8f9b55de9950

---
+++ CS202/T/final/FinalProject/FinalProject/EditRuleWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Contract;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FinalProject
+{
+	public partial class EditRuleWindow : Window
+	{
+		public IRule ReturnData = null;
+		bool validConfig = true;
+		
+		public EditRuleWindow(IRule passedInData)
+		{
+			InitializeComponent();
+
+			ReturnData = passedInData;
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			ruleNameTextBlock.Text = ReturnData.Name;
+			ruleConfigTextBox.Text = ReturnData.Config;
+		}
+
+		private void okButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = validConfig;
+		}
+
+		private void ruleConfigTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			try
+			{
+				string newConfig = ReturnData.Name + " " + ruleConfigTextBox.Text;
+
+				RuleFactory f = new RuleFactory();
+				ReturnData = f.StringToIRuleConverter(newConfig);
+			}
+			catch
+			{
+				validConfig = false;
+				MessageBox.Show("Invalid config!");
+			}
+		}
+	}
+}
---
+++ CS202/T/final/FinalProject/OneSpaceRule/OneSpace.cs
@@ -0,0 +1,20 @@
+﻿using Contract;
+
+namespace OneSpaceRule
+{
+	public class OneSpace : IRule
+	{
+		public string Name => "OneSpace";
+
+		public string Config => "";
+
+		public string Parse { get; set; }
+
+		public string Rename(string inp, int fileIndex)
+		{
+			return (string.Join(" ", inp.Split(" ", StringSplitOptions.RemoveEmptyEntries)));
+		}
+
+		public object Clone() => MemberwiseClone();
+	}
+}
