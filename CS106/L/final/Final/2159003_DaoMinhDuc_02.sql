6b703880ef9efe5a4ddfb7a63f741dd3f217cfd9
Đức Đào <duc.dao.431@gmail.com>
2022-11-23 06:38:10 +0000

    106.L.final.03
    Submission

---
+++ CS106/L/final/Final/2159003_DaoMinhDuc_02.sql
@@ -0,0 +1,52 @@
+﻿-- Họ tên: Đào Minh Đức
+-- ID: 2159003
+-- Mã đề: 2
+
+use QLNhomNghienCuu
+go
+
+-- 1. Viết truy vấn cho biết (mã nhóm, tên nhóm, số thành viên) của nhóm nghiên cứu nào có số thành viên (đang hoạt động) nhiều nhất.
+select NNC.MaNhom, NNC.TenNhom, count(TV.MaGV) as SoThanhVien
+from NhomNC as NNC,
+	ThanhVien_NNC as TV
+where NNC.MaNhom = TV.MaNhom
+group by NNC.MaNhom, NNC.TenNhom
+having count(TV.MaGV) >= ALL (select count(TV.MaGV)
+								from NhomNC as NNC,
+									ThanhVien_NNC as TV
+								where NNC.MaNhom = TV.MaNhom
+								group by NNC.MaNhom, NNC.TenNhom)
+go
+
+-- 2. Viết truy vấn cho biết nhóm nghiên cứu nào có tất cả thành viên tham gia đều là tiến sĩ.
+select NNC.TenNhom
+from NhomNC as NNC
+where not exists (select *
+					from ThanhVien_NNC as TV
+					where NNC.MaNhom = TV.MaNhom
+						and TV.MaGV not in (select MaGV
+											from GiangVien as GV,
+												HocVi as HV
+											where GV.HocVi = HV.MaHV
+												and HV.TenHV = N'Tiến sĩ'))
+go
+
+
+
+-- 3. Viết stored procedure thực hiện cập nhật học vị cho 1 giảng viên
+-- • Input: mã giảng viên, mã học vị
+-- • Gọi thực thi stored procedure
+-- drop procedure CapNhatHocVi
+create procedure CapNhatHocVi
+	@MaGV varchar(5),
+	@MaHV varchar(3)
+as
+begin
+	update GiangVien
+	set HocVi = @MaHV
+	where MaGV = @MaGV
+end
+go
+
+execute CapNhatHocVi 'GV01', 'TS'
+go
