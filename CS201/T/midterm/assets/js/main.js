deadedd0e43efb8b4efea1574df6904bcb1044e6
Đức Đào <duc.dao.431@gmail.com>
2023-02-02 17:48:22 +0700

    201.T.midterm.09
    add Topics

--- CS201/T/midterm/assets/js/main.js
+++ CS201/T/midterm/assets/js/main.js
@@ -8,18 +8,18 @@ console.log("main.js loaded");
 
 /* -------- Catalogue content ------- */
 
-let ctlContentTitle = [["Healthcare Products & Solutions", "Industrial Metrology", "Optical Processing Machine", "Encoders", "Industrial Lenses", "Material Processing Solutions & Robot Vision", "Optical Components & Materials", "Surveying Instruments", "FPD Lithography Systems", "Semiconductor Systems", "MEMS Steppers & Both Side Measurement System"], ["Digital SLR Cameras", "Mirrorless Cameras", "Compact Digital Cameras", "NIKKOR Lenses", "Speedlights", "Software", "Sport Optics", "Laser Rangefinder", "Accessories", "Ophthalmic Lenses"]];
+let catalogueContentTitle = [["Healthcare Products & Solutions", "Industrial Metrology", "Optical Processing Machine", "Encoders", "Industrial Lenses", "Material Processing Solutions & Robot Vision", "Optical Components & Materials", "Surveying Instruments", "FPD Lithography Systems", "Semiconductor Systems", "MEMS Steppers & Both Side Measurement System"], ["Digital SLR Cameras", "Mirrorless Cameras", "Compact Digital Cameras", "NIKKOR Lenses", "Speedlights", "Software", "Sport Optics", "Laser Rangefinder", "Accessories", "Ophthalmic Lenses"]];
 
-let ctlContentUrl = [["https://www.healthcare.nikon.com/en/", "https://industry.nikon.com/en-aom/", "https://www.nikon.com/products/optical-processing-machine/", "https://www.nikon.com/products/encoder/", "https://www.nikon.com/products/industrial-lenses/", "https://ngpd.nikon.com/en/", "https://www.nikon.com/products/components/", "https://www.nikon-trimble.co.jp/english/", "https://www.nikon.com/products/fpd/lineup/", "https://www.nikon.com/products/semi/lineup/", "https://www.nikonprecision.com/products-and-technology/steppers-and-metrology-solutions-for-mems-markets/"], ["https://imaging.nikon.com/lineup/dslr/", "https://imaging.nikon.com/lineup/mirrorless/", "https://imaging.nikon.com/lineup/compact/", "https://imaging.nikon.com/lineup/lens/", "https://imaging.nikon.com/lineup/speedlight/", "https://imaging.nikon.com/lineup/software/", "https://imaging.nikon.com/lineup/sportoptics/", "https://imaging.nikon.com/lineup/sportoptics/laser/", "https://imaging.nikon.com/lineup/accessory/", "https://www.nikonlenswear.com/"]];
+let catalogueContentUrl = [["https://www.healthcare.nikon.com/en/", "https://industry.nikon.com/en-aom/", "https://www.nikon.com/products/optical-processing-machine/", "https://www.nikon.com/products/encoder/", "https://www.nikon.com/products/industrial-lenses/", "https://ngpd.nikon.com/en/", "https://www.nikon.com/products/components/", "https://www.nikon-trimble.co.jp/english/", "https://www.nikon.com/products/fpd/lineup/", "https://www.nikon.com/products/semi/lineup/", "https://www.nikonprecision.com/products-and-technology/steppers-and-metrology-solutions-for-mems-markets/"], ["https://imaging.nikon.com/lineup/dslr/", "https://imaging.nikon.com/lineup/mirrorless/", "https://imaging.nikon.com/lineup/compact/", "https://imaging.nikon.com/lineup/lens/", "https://imaging.nikon.com/lineup/speedlight/", "https://imaging.nikon.com/lineup/software/", "https://imaging.nikon.com/lineup/sportoptics/", "https://imaging.nikon.com/lineup/sportoptics/laser/", "https://imaging.nikon.com/lineup/accessory/", "https://www.nikonlenswear.com/"]];
 
-const ctlContentImgUrlPrefix = "https://www.nikon.com/img/index/pic_product_";
-const ctlContentImgUrlPrefix_tab = ["corp", "indi"];
-const ctlContentImgUrlSuffix = ".png";
+const catalogueContentImgUrlPrefix = "https://www.nikon.com/img/index/pic_product_";
+const catalogueContentImgUrlPrefix_tab = ["corp", "indi"];
+const catalogueContentImgUrlSuffix = ".png";
 
-function generateCtlContentItem(tabIndex, index) {
+function generatecatalogueContentItem(tabIndex, index) {
 	let contentItem = document.createElement('a');
-	contentItem.classList.add('ctl-content-item');
-	contentItem.href = ctlContentUrl[tabIndex][index];
+	contentItem.classList.add('catalogue-content-item');
+	contentItem.href = catalogueContentUrl[tabIndex][index];
 	
 	let contentImg = document.createElement('img');
 	
@@ -40,44 +40,44 @@ function generateCtlContentItem(tabIndex, index) {
 		index -= 1;
 	}
 	
-	contentImg.src = ctlContentImgUrlPrefix + ctlContentImgUrlPrefix_tab[tabIndex] + "_" + ((imgIndex + 1) < 10 ? "0" + String(imgIndex + 1) : String(imgIndex + 1)) + ctlContentImgUrlSuffix;
+	contentImg.src = catalogueContentImgUrlPrefix + catalogueContentImgUrlPrefix_tab[tabIndex] + "_" + ((imgIndex + 1) < 10 ? "0" + String(imgIndex + 1) : String(imgIndex + 1)) + catalogueContentImgUrlSuffix;
 	contentItem.appendChild(contentImg);
 	
 	let contentTitle = document.createElement('h3');
-	contentTitle.innerText = ctlContentTitle[tabIndex][index];
+	contentTitle.innerText = catalogueContentTitle[tabIndex][index];
 	contentItem.appendChild(contentTitle);
 	
 	return contentItem;
 }
 
-function generateCtlContent() {
-	let ctlContent = document.querySelector('.ctl-content');
+function generatecatalogueContent() {
+	let catalogueContent = document.querySelector('.content-grid');
 	
 	// Remove all current content items
-	while (ctlContent.firstChild) {
-		ctlContent.removeChild(ctlContent.firstChild);
+	while (catalogueContent.firstChild) {
+		catalogueContent.removeChild(catalogueContent.firstChild);
 	}
 	
-	for (let i = 0; i < ctlContentTitle[ctlCurrentTab].length; i++) {
-		ctlContent.appendChild(generateCtlContentItem(ctlCurrentTab, i));
-		console.log("ctlContentItem " + i + " generated");
+	for (let i = 0; i < catalogueContentTitle[catalogueCurrentTab].length; i++) {
+		catalogueContent.appendChild(generatecatalogueContentItem(catalogueCurrentTab, i));
+		console.log("catalogueContentItem " + i + " generated");
 	}
 }
 
 /* ---------- Catalogue tab --------- */
 
-let ctlCurrentTab = 0;
-let ctlTab = document.querySelectorAll('.ctl-tab');
+let catalogueCurrentTab = 0;
+let catalogueTab = document.querySelectorAll('.catalogue-tab');
 
 // Set first tab to be selected
-ctlTab[ctlCurrentTab].classList.add('is-selected');
-generateCtlContent();
+catalogueTab[catalogueCurrentTab].classList.add('is-selected');
+generatecatalogueContent();
 
 // Add EL to each button
-ctlTab.forEach(button => {
+catalogueTab.forEach(button => {
 	  button.addEventListener('click', () => {
 		// Remove all current is-selected classes
-		ctlTab.forEach(button => {
+		catalogueTab.forEach(button => {
 			button.classList.remove('is-selected');
 		});
 		
@@ -85,9 +85,9 @@ ctlTab.forEach(button => {
 		button.classList.add('is-selected');
 		
 		// Get the index of the clicked button
-		ctlCurrentTab = Array.from(ctlTab).indexOf(button);
+		catalogueCurrentTab = Array.from(catalogueTab).indexOf(button);
 		
 		// Generate content
-		generateCtlContent();
+		generatecatalogueContent();
 	});
 });
