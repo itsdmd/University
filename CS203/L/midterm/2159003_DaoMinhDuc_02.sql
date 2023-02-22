61674d7838217ee0ea9061e0482a96c75c4bce0d
Đức Đào <duc.dao.431@gmail.com>
2023-02-22 14:28:19 +0700

    203.L.midterm.05
    Q2

--- CS203/L/midterm/2159003_DaoMinhDuc_02.sql
+++ CS203/L/midterm/2159003_DaoMinhDuc_02.sql
@@ -6,6 +6,9 @@
 use QLNhomNghienCuu
 go
 
+set dateformat 'dmy'
+go
+
 -- 1. Viết stored procedure thực hiện thêm mới 1 nhóm nghiên cứu, biết rằng:
 -- - Trưởng nhóm nghiên cứu phải cùng khoa với nhóm nghiên cứu và phải đạt học vị Thạc sỹ trở lên.
 -- - Trưởng nhóm được phân công không được đang kiêm nhiệm trưởng cho nhóm nào khác (chỉ làm trưởng 1 nhóm, không tính nhóm trong quá khứ)
@@ -62,6 +65,9 @@ begin
 	
 	-- Them nhom nghien cuu
 	insert into NhomNC values (@MaNhom, @TenNhom, GETDATE(), @MaGV, @MaKhoa_NNC)
+	
+	-- Them thanh vien
+	insert into ThanhVien_NNC values (@MaNhom, @MaGV, GETDATE(), NULL)
 end
 go
 
@@ -69,4 +75,117 @@ go
 -- go
 
 exec sp_ThemNhomNC 'NH10', N'Nhom nghien cuu Vi Sinh', 'VS', 'GV08'
+go
+
+
+
+-- 2. Viết thủ tục xuất thông tin nghiên cứu của các nhóm của 1 khoa cụ thể như mẫu sau:
+-- Mã nhóm NC:
+-- Ngày lập nhóm:
+-- Số thành viên hiện tại:
+-- Danh sách các đề tài nghiên cứu phụ trách:
+-- Mã đề tài		Ngày bắt đầu		Tình trạng			Tên GV chủ nhiệm
+-- DT01				1/1/2022			Đang thực hiện		Tuấn
+-- DT02				13/2/2021			Đã nghiệm thu		Huy
+-- ----------------------------------
+-- Mã nhóm NC:
+-- Ngày lập nhóm:
+-- Số thành viên hiện tại:
+-- etc.
+-- Biết rằng: tình trạng đề tài có giá trị (đã nghiệm thu, đang thực hiện)
+
+create
+-- alter
+procedure sp_XuatThongTinNghienCuu
+	@MaKhoa nvarchar(10)
+as
+begin
+	-- Khoa khong ton tai
+	if (select makhoa
+		from Khoa
+		where makhoa = @MaKhoa) is NULL
+	begin
+		RAISERROR('Khoa khong ton tai', 15, 1)
+		return
+	end
+	
+	-- Xuat thong tin
+	declare @MaNhom nvarchar(10)
+	declare @TenNhom nvarchar(50)
+	declare @NgayLap date
+	declare @TruongNhom nvarchar(10)
+	declare @SoThanhVien int
+	declare @MaDT nvarchar(10)
+	declare @NgayBD date
+	declare @NgayNT date
+	declare @TinhTrang nvarchar(20)
+	declare @ChuNhiemDT nvarchar(10)
+	declare @TenGV nvarchar(50)
+	
+	declare cur1 cursor for
+	select MaNhom, TenNhom, NgayLapNhom, TruongNhom, MaKhoa
+	from NhomNC
+	where MaKhoa = @MaKhoa
+	
+	-- SoThanhVien: Dem trong ThanhVien_NNC, neu NgayRoiNhom = NULL thi +1
+	set @SoThanhVien = (select count(*)
+						from ThanhVien_NNC
+						where MaNhom = @MaNhom
+							and NgayRoiNhom is NULL)
+	
+	open cur1
+	fetch next from cur1 into @MaNhom, @TenNhom, @NgayLap, @TruongNhom, @MaKhoa
+	
+	while @@FETCH_STATUS = 0
+	begin
+		print 'Ma nhom NC: ' + @MaNhom
+		print 'Ngay lap nhom: ' + convert(varchar(10), @NgayLap)
+		print 'So thanh vien hien tai: ' + convert(varchar(5), @SoThanhVien)
+		
+		print 'Danh sach cac de tai nghien cuu phu trach:'
+		print 'Ma de tai		Ngay bat dau		Tinh trang			Ten GV chu nhiem'
+		
+		declare cur2 cursor for
+		select MaDT, NgayBD, NgayNT, ChuNhiemDT
+		from DeTaiNCC
+		where MaNNC = @MaNhom
+		
+		open cur2
+		fetch next from cur2 into @MaDT, @NgayBD, @NgayNT, @ChuNhiemDT
+		
+		while @@FETCH_STATUS = 0
+		begin
+			-- Ngay nghiem thu = null => dang thuc hien
+			if (select NgayNT
+				from DeTaiNCC
+				where MaNNC = @MaNhom) is NULL
+				set @TinhTrang = N'Đang thực hiện'
+			else
+				set @TinhTrang = N'Đã nghiệm thu'
+				
+			set @TenGV = (select HoTen
+					  from GiangVien
+					  where MaGV = @ChuNhiemDT)
+			
+			print @MaDT + '		' + convert(varchar(10), @NgayBD) + '		    ' + @TinhTrang + '		' + @TenGV
+			fetch next from cur2 into @MaDT, @NgayBD, @TinhTrang, @TenGV
+		end
+		
+		close cur2
+		deallocate cur2
+		
+		print '----------------------------------'
+		
+		fetch next from cur1 into @MaNhom, @TenNhom, @NgayLap, @TruongNhom, @MaKhoa
+	end
+	
+	close cur1
+	deallocate cur1
+end
+go
+
+-- insert into DeTaiNCC values ('2023.VS.01', 'NH10', 'Test', '2023-02-01', NULL, NULL, '40000000.0000', 'GV08', NULL)
+-- go
+
+exec sp_XuatThongTinNghienCuu 'VS'
 go
