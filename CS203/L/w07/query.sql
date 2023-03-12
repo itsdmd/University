0551b71e7edb39227e0d7fddb05194e8e6bea581
Đức Đào <duc.dao.431@gmail.com>
2023-03-12 10:38:28 +0700

    203.L.07.01
    btvn

--- CS203/L/w07/query.sql
+++ CS203/L/w07/query.sql
@@ -1,6 +1,76 @@
 use QLThuVien
 go
 
+-- 1. Ngày mượn sách phải là ngày hiện tại
+--		T	X	S
+-- PM	+	-	-
+create
+-- alter
+trigger tg_cau1
+on PhieuMuon
+for insert
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i
+		where i.NgayMuon <> convert(date, getdate())
+	)
+	begin
+		raiserror('Ngay muon phai la ngay hien tai', 15, 1)
+		rollback
+	end
+end
+go
+
+-- 2. Ngày trả sách phải là ngày hiện tại.
+--		T	X	S
+-- PT	+	-	-
+create
+-- alter
+trigger tg_cau2
+on PhieuTra
+for insert
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i
+		where i.NgayTra <> convert(date, getdate())
+	)
+	begin
+		raiserror('Ngay tra phai la ngay hien tai', 15, 1)
+		rollback
+	end
+end
+go
+
+-- 3. Tình trạng sách chỉ bao gồm : "đang được mượn" hoặc "có thể cho mượn"
+--		T	X	S
+-- CS	+	-	+(tinhtrang)
+create
+-- alter
+trigger tg_cau3
+on CuonSach
+for insert, update
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i
+		where i.TinhTrang <> N'đang được mượn'
+			and i.TinhTrang <> N'có thể cho mượn'
+	)
+	begin
+		raiserror(N'Tinh trang sach chi bao gom: "đang được mượn" hoac "có thể cho mượn"', 15, 1)
+		rollback
+	end
+end
+go
+
 -- 4. Đọc giả muốn mượn sách phải từ 18 tuổi trở lên.
 --		T	X	S
 -- PM	+	-	+(madg)
@@ -57,61 +127,187 @@ go
 insert into DauSach(isbn) values ('12345678900')
 go
 
--- 6. SoLantra = số phiếu trả của phiếu mượn tương ứng.
+-- 6. Thêm thuộc tính "SoLanTra" vào bảng PhieuMuon. SoLantra = số phiếu trả của phiếu mượn tương ứng.
 --		T	X	S
--- PM	-	-	-
+-- PM	+	-	+(solantra)
 -- PT	+	+	+(mapm)
-create
--- alter
-trigger tg_cau6_i
-on PhieuTra
-for insert
-as
+if not exists
+(
+	select *
+	from sys.columns
+	where name = 'SoLanTra'
+		and object_id = object_id('dbo.PhieuMuon')
+)
 begin
-	update PhieuMuon
-	set SoLanTra = SoLanTra +
-	(
-		select count(*)
-		from PhieuTra
-		where mapm = PhieuMuon.mapm
-	)
-	from inserted i
-	where PhieuMuon.mapm = i.mapm
+	print 'add column SoLanTra'
+	alter table PhieuMuon
+	add SoLanTra int
+		default 0
 end
 go
 
 create
 -- alter
-trigger tg_cau6_u
+trigger tg_cau6_1
+on PhieuMuon
+for insert, update
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i
+		where i.SoLanTra <> (select count(*) from PhieuTra where mapm = i.mapm)
+	)
+	begin
+		raiserror('So lan tra phai bang so phieu tra cua phieu muon tuong ung', 15, 1)
+		rollback
+	end
+end
+go
+
+create
+-- alter
+trigger tg_cau6_2
 on PhieuTra
 for update
 as
 begin
-	update PhieuMuon
-	set SoLanTra =
+	if exists
 	(
 		select count(*)
-		from PhieuTra
-		where mapm = PhieuMuon.mapm
+		from inserted i,
+			PhieuMuon p
+		where i.mapm = p.mapm
+			and p.SoLanTra <> (select count(*) from PhieuTra where mapm = p.mapm)
 	)
+	begin
+		raiserror('So lan tra phai bang so phieu tra cua phieu muon tuong ung', 15, 1)
+		rollback
+	end
+end
+
+-- 7. Thêm thuộc tính "NgayTraDuKien" vào bảng CT_PhieuTra. Ngày trả dự kiến = ngày mượn + số ngày quy định được mượn.
+-- 		T	X	S
+-- PM	-	-	+(ngaymuon)
+-- CTPM	-	-	+(songayquydinh)
+-- PT	-	-	+(mapm)
+-- CTPT	+	-	+(ngaytradukien)
+
+if not exists
+(
+	select *
+	from sys.columns
+	where name = 'NgayTraDuKien'
+		and object_id = object_id(N'dbo.CT_PhieuTra')
+)
+begin
+	print 'add column NgayTraDuKien'
+	alter table CT_PhieuTra
+	add NgayTraDuKien date
+		default null
 end
 go
 
 create
 -- alter
-trigger tg_cau6_d
-on PhieuTra
-for delete
+trigger tg_cau7_1
+on CT_PhieuTra
+for insert, update
 as
 begin
-	update PhieuMuon
-	set SoLanTra = SolanTra -
+	if exists
 	(
 		select count(*)
-		from PhieuTra
-		where mapm = PhieuMuon.mapm
+		from inserted i,
+			PhieuMuon pm,
+			CT_PhieuMuon ctpm,
+			PhieuTra pt
+		where i.mapt = pt.mapt
+			and pt.mapm = pm.mapm
+			and pm.mapm = ctpm.mapm
+			and i.NgayTraDuKien <> dateadd(day, ctpm.SoNgayQuyDinh, pm.NgayMuon)
 	)
-	from deleted d
-	where PhieuMuon.mapm = d.mapm
+	begin
+		raiserror('Ngay tra du kien phai la ngay muon cong voi so ngay quy dinh', 15, 1)
+		rollback
+	end
+end
+go
+
+create
+-- alter
+trigger tg_cau7_2
+on PhieuTra
+for update
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i,
+			PhieuMuon pm,
+			CT_PhieuMuon ctpm,
+			CT_PhieuTra ctpt
+		where i.mapm = pm.mapm
+			and pm.mapm = ctpm.mapm
+			and ctpt.NgayTraDuKien <> dateadd(day, ctpm.SoNgayQuyDinh, pm.NgayMuon)
+	)
+	begin
+		raiserror('Ngay tra du kien phai la ngay muon cong voi so ngay quy dinh', 15, 1)
+		rollback
+	end
+end
+go
+
+create
+-- alter
+trigger tg_cau7_3
+on PhieuMuon
+for update
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i,
+			CT_PhieuMuon ctpm,
+			PhieuTra pt,
+			CT_PhieuTra ctpt
+		where i.mapm = ctpm.mapm
+			and i.mapm = pt.mapm
+			and pt.mapt = ctpt.mapt
+			and ctpt.NgayTraDuKien <> dateadd(day, ctpm.SoNgayQuyDinh, i.NgayMuon)
+	)
+	begin
+		raiserror('Ngay tra du kien phai la ngay muon cong voi so ngay quy dinh', 15, 1)
+		rollback
+	end
+end
+go
+
+create
+-- alter
+trigger tg_cau7_4
+on CT_PhieuMuon
+for update
+as
+begin
+	if exists
+	(
+		select count(*)
+		from inserted i,
+			PhieuMuon pm,
+			PhieuTra pt,
+			CT_PhieuTra ctpt
+		where i.mapm = pm.mapm
+			and pt.mapt = ctpt.mapt
+			and pt.mapm = pm.mapm
+			and ctpt.NgayTraDuKien <> dateadd(day, i.SoNgayQuyDinh, pm.NgayMuon)
+	)
+	begin
+		raiserror('Ngay tra du kien phai la ngay muon cong voi so ngay quy dinh', 15, 1)
+		rollback
+	end
 end
 go
