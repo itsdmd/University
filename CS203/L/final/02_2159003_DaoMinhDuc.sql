a8169bc2cd78b5f07d7cc0d6d11ad1c09cc3c456
Đức Đào <duc.dao.431@gmail.com>
2023-03-22 13:32:20 +0700

    203.final.01
    Q.1

---
+++ CS203/L/final/02_2159003_DaoMinhDuc.sql
@@ -0,0 +1,46 @@
+﻿-- Họ tên: Đào Minh Đức
+-- MSSV: 2159003
+-- Mã đề: 
+
+use QLNhomNghienCuu
+go
+
+-- 1. Viết hàm truyền vào MaGV cho biết học vị của giảng viên này
+-- Tên function: UF_D1_C1
+-- Input: MaGV
+-- Output: số đề tài chưa nghiệm thu (xep loai = null)
+
+create
+-- alter
+-- drop
+function UF_D1_C1(@MaGV char(5))
+returns int
+as
+begin
+	declare @result int
+	select @result = count(*)
+	from NhomNC nnc,
+		ThanhVien_NNC tv,
+		GiangVien gv,
+		DeTaiNCC dt
+	where tv.MaGV = gv.MaGV
+		and tv.MaNhom = nnc.MaNhom
+		and gv.MaGV = @MaGV
+		and dt.MaNNC = nnc.MaNhom
+		and dt.XepLoai is null
+	return @result
+end
+go
+
+-- test
+select dbo.UF_D1_C1('GV03')
+go
+
+-- 2. Vẽ bảng tầm ảnh hưởng và chọn 1 trigger để cài cho RBTV sau:
+-- “Giảng viên chủ nhiệm đề tài phải có học vị Tiến Sĩ”
+-- Tên trigger: UTR_D1_2
+
+-- 3. Cập nhật giảng viên chủ nhiệm là một thành viên là tiến sĩ trong nhóm nghiên cứu nếu vi
+-- phạm điều kiện ở câu 2 (Lưu ý: nếu không có giáo viên nào thỏa thì xóa đề tài NC và sản
+-- phẩm của đề tài đó)
+-- Tên trigger: UTR_D1_C3
--- CS203/L/final/query.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Họ tên: Đào Minh Đức
--- MSSV: 2159003
--- Mã đề: 
-
-use QLNhomNghienCuu
-go
-
--- 
