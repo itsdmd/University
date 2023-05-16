7743d89a7d9ab5463e1a3132d3cb5750a3983751
Đức Đào <duc.dao.431@gmail.com>
2023-05-16 15:28:18 +0700

    204.L.04.01

--- CS204/L/w04/index.php
+++ CS204/L/w04/index.php
@@ -6,15 +6,58 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-include "query.php";
+$query_string = "";
+$query_bind = "";
 
-$statement = $conn->prepare($sql);
-$statement->execute();
-$results = $statement->get_result();
+// if the author name is set in the form, use it in the query
+if (isset($_POST['author'])) {
+    $query_string = "SELECT post_title, display_name
+        FROM wp_posts
+        JOIN wp_users ON wp_users.ID = wp_posts.post_author
+        WHERE post_type = 'post'
+            AND display_name = ?
+        LIMIT 20";
+    $query_bind = $_POST['author'];
+}
+// else show all
+else {
+    $query_string = "SELECT post_title, display_name
+        FROM wp_posts
+        JOIN wp_users ON wp_users.ID = wp_posts.post_author
+        WHERE post_type = 'post'
+        LIMIT 20";
+    $query_bind = "";
+}
+
+function query($conn, $query, $bind = "") {
+    $statement = $conn->prepare($query);
+
+    if ($bind) {
+        $statement->bind_param("s", $bind);
+    }
+
+    $statement->execute();
+    return $statement->get_result();
+}
+
 ?>
 
-<pre>
+<h1>Posts</h1>
+<!-- form to search by author name -->
+<form action="index.php" method="post">
+    <input type="text" name="author" placeholder="Author name">
+    <input type="submit" value="Search">
+</form>
+
+<ul>
     <?php
-    print_r($results->fetch_all(MYSQLI_ASSOC));
+    $posts = query($conn, $query_string, $query_bind)->fetch_all(MYSQLI_ASSOC);
+
+    foreach ($posts as $post) :
     ?>
-</pre>
+        <li>
+            <h2><?= $post['post_title'] ?></h2>
+            <p>by <b><?= $post['display_name'] ?></b></p>
+        </li>
+    <?php endforeach; ?>
+</ul>
--- CS204/L/w04/query.php
+++ CS204/L/w04/query.php
@@ -1,2 +1,6 @@
 <?php
-$sql = "SELECT post_title FROM wp_posts LIMIT 20";
+$q1 = "SELECT post_title, display_name
+    FROM wp_posts
+    JOIN wp_users ON wp_users.ID = wp_posts.post_author
+    WHERE post_type = 'post'
+    LIMIT 20";
