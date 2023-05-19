1098cd12fbcd1a8cb74abf55d822a7669c8399cb
Đức Đào <duc.dao.431@gmail.com>
2023-05-19 15:16:47 +0700

    [add] 204.T.04

---
+++ CS204/T/w04/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+include "query.php";
+include "secrets.php";
+?>
+
+<head>
+    <title>Week 04</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+
+        <pre>
+            <?php
+            $q1 = "SELECT s.id AS student_id, s.s_name AS student_name, c.c_name AS class_name, t.t_name AS teacher_name
+            FROM enrollment e
+            JOIN classes c on c.id = e.c_id
+            JOIN students s on s.id = e.s_id
+            JOIN teachers t on t.id = e.t_id";
+
+            $posts = query($sconn, $q1)->fetch_all(MYSQLI_ASSOC);
+
+            print_r($posts);
+            ?>
+        </pre>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
