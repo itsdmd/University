c5afc71a0569bb399a1612ce8e4113c1db13f8cc
Đức Đào <duc.dao.431@gmail.com>
2023-06-09 16:29:52 +0700

    204.T.07.03

--- CS204/T/w07/controllers/Controller.php
+++ CS204/T/w07/controllers/Controller.php
@@ -7,10 +7,11 @@ class Controller {
     public $files;
 
     public function __construct() {
-
+        $this->conn = DB::getConn();
         $this->req = $_POST;
         $this->params = $_GET;
         $this->files = $_FILES;
+        echo "<pre>", var_dump($this->conn), "</pre>";
         echo "Parent Conroller class instantiated.";
     }
 }
--- CS204/T/w07/index.php
+++ CS204/T/w07/index.php
@@ -1,6 +1,10 @@
 <?php
 define("ROOT", substr($_SERVER['PHP_SELF'], 0, -9));
 
+include "core/DB.php";
+DB::createInstance();
+DB::connect();
+
 include "controllers/Controller.php";
 include "controllers/Home.php";
 include "controllers/User.php";
